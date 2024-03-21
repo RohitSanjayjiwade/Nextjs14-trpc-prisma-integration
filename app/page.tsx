8239cbf1e332e374955310c6fae008c83797ba37
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-03-21 14:05:29 +0530

    Complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
 "use client";
 import { trpc } from "@/server/client";
+import { useState } from "react";
 
 export default function Home() {
 
   const getUsers = trpc.user.getUsers.useQuery();
+  const addUsers = trpc.user.addUser.useMutation({
+    onSettled: () => {
+      getUsers.refetch();
+    }
+  });
+
+  const [name, setName] = useState<string>("");
+  const [race, setRace] = useState<string>("");
 
   return (
     <main className="flex flex-col items-center justify-between p-24">
-      {JSON.stringify(getUsers.data)}
+      {/*{JSON.stringify(getUsers.data)}*/}
+      <div className="flex flex-col gap-3">
+        Name: <input value={name} type="text" onChange={(e) => setName(e.target.value)} className="border" />
+        Race: <input value={race} type="text" onChange={(e) => setRace(e.target.value)} className="border" />
+        <button className="border" onClick={()=> addUsers.mutate({name, race})}>Add</button>
+      </div>
     </main>
   );
 }
