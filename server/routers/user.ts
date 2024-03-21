5530bd3550ea5347b2b3acd3248da1008f76160b
Rohit Jiwade <rohitjiwade2019@gmail.com>
2024-03-21 13:47:05 +0530

    Prisma DB Calls

--- server/routers/user.ts
+++ server/routers/user.ts
@@ -1,18 +1,25 @@
 import { z } from "zod";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
 import { procedure, router } from "../trpc";
 
 export const userRouter = router({
-	getUsers: procedure.query(() => {
-		return [
-			{ name: "Golmar", race: "Orc"},
-			{ name: "Grom", race: "Orc"},
-		];
+	getUsers: procedure.query(async() => {
+		return await prisma.user.findMany();
 	}),
 
 	addUser: procedure
 	.input(z.object({name: z.string(), race: z.string()}))
-	.mutation((opts) =>{
+	.mutation(async(opts) =>{
 		const { input } = opts;
+		await prisma.user.create({
+			data: {
+				name: input.name,
+				race: input.race,
+			}
+		})
 		// TODO: Call prisma add user method
 	})
 });
